4888e536ab5fdcc5426f21ebf872d758c715bd95
deep <raxizel@gmail.com>
2015-03-16 22:22:14 +0545

    title changed from metis to cms

--- application/views/admin/templates/metis/includes/header.php
+++ application/views/admin/templates/metis/includes/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Metis</title>
+  <title>CMS</title>
 
   <!--Mobile first-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
